0f9e7751140196dff050e58f87e93386feadcbe2
juancleon <31444927+juancleon@users.noreply.github.com>
2019-04-19 23:31:08 -0700

    Add files via upload

--- models/school.model.js
+++ models/school.model.js
@@ -8,15 +8,21 @@ let School = new Schema({
     schoolUrl: {
         type: String
     },
+    state: {
+        type: String
+    },
     zipCode: {
         type: Number
     },
     costOfLivingIndex: {
         type: Number
     },
+    shiftedCostOfLivingIndex: {
+        type: Number
+    },
     programsOfferedArray: {
         type: [String]
     }
 });
-School.index({programsOffered: 'text'});
+//School.index({programsOfferedArray: 'text'});
 module.exports = mongoose.model('schools', School);/*Parameters are (MongoDB Collection name, name of Schema object defined in file)*/
